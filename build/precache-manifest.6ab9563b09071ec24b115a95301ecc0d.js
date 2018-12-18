c6b773bc781266f9c39c3e2a45544a6b1c54c87d
himanshu1603 <himanshu30ber@gmail.com>
2018-12-19 04:06:11 +0530

    Second commit

---
+++ build/precache-manifest.6ab9563b09071ec24b115a95301ecc0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/static/media/spinner.75ae2ba2.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "314658d291e828e780b5",
+    "url": "/static/js/main.314658d2.chunk.js"
+  },
+  {
+    "revision": "b0eb63c9be6ff2453109",
+    "url": "/static/js/1.b0eb63c9.chunk.js"
+  },
+  {
+    "revision": "314658d291e828e780b5",
+    "url": "/static/css/main.34a994d1.chunk.css"
+  },
+  {
+    "revision": "6d4f9c5f4fcc455291536e0889d33599",
+    "url": "/index.html"
+  }
+];
